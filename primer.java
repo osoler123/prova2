af762eec8fcc4d0c15050098d4a12b641f739168
osoler <osoler@almata.cat>
2022-09-28 17:02:50 +0200

    Els 2 fitxers han sigut millorats

--- primer.java
+++ primer.java
@@ -1,5 +1,12 @@
 public class primer{
 public static void main(String[] args){
 System.out.println("Primer java");
+
+En aquest java realitzarem una aplicacio mobil
+Hem modificat i millorat el codi per una nova versio que creiem que sera 
+
+fantastica
+
+
 }
 }
---
+++ segon.java
@@ -0,0 +1,2 @@
+Aquest es un nou fitxer que millora el programa
+Hem millorat el codi del segon java com el primer
